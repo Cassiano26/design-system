41e877f3268f8e5f919e53fdb1d9ea659da88466
Cassiano26 <candidocassiano2@gmail.com>
2024-11-01 16:30:00 -0300

    build: add stitchs config.

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,13 @@
-import { colors } from '@ignite-ui/tokens'
+import { styled } from './styles'
+
+const Buttom = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$md',
+  padding: '$4',
+  height: '$10',
+})
 
 export function App() {
-  return <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
+  return <Buttom>Hello world</Buttom>
 }
