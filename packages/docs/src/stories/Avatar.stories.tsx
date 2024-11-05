c9fa24ac6fc409c7be381bd6931e82960810ead2
Cassiano26 <candidocassiano2@gmail.com>
2024-11-05 14:51:11 -0300

    feat: Avatar component.

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data Display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/Cassiano26.png',
+    alt: 'Cassiano Candido da Silva',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallBack: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps } from 'react'
+import { AvatarContainer, AvatarImage, AvatarFallBack } from './style'
+import { User } from 'phosphor-react'
+
+export type AvatarProps = ComponentProps<typeof AvatarImage>
+
+export function Avatar(props: AvatarProps) {
+  return (
+    <AvatarContainer>
+      <AvatarImage {...props} />
+      <AvatarFallBack delayMs={600}>
+        <User />
+      </AvatarFallBack>
+    </AvatarContainer>
+  )
+}
